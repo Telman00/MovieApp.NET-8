067b5f94913df7d292d4bd5b435cc4a43df2821c
Telman00 <telmanabidov00@gmail.com>
2025-01-15 16:34:54 +0400

    Controllers_Views

--- obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43dc48073f60a9041cbdb75e84ed0a5ca115d67f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
